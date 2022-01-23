eed6cbd1757ae731518f96b2a8fc5b57297d8544
Laz <lazgerasimov@gmail.com>
2022-01-23 12:31:15 +0000

    add

---
+++ 02. Data Types and Variables/02. Exercise/02-CharsToString.js
@@ -0,0 +1,12 @@
+function chars(char1, char2, char3) {
+
+    console.log(`${char1}${char2}${char3}`)
+
+}
+chars("a", "b", "c")
+
+
+
+// or we can do it with: 
+
+// let converted = "" + char1 + char2 + char3
