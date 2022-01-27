a30652878d15c317ad62f187db0650cd9f8f3288
Laz <lazgerasimov@gmail.com>
2022-01-27 12:48:31 +0000

    add

---
+++ 03-Arrays/Lab/01-Sum FIrst and Last Array.js
@@ -0,0 +1,9 @@
+function sumNumbers(numbers) {
+
+    let first = numbers[0];
+    let last = numbers[numbers.length - 1];
+    let sum = first + last; 
+
+    console.log(sum)
+}
+sumNumbers([20,30,40,55])
