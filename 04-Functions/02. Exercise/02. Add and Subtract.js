827e2c1774e7cd90070e1f1b76581d1f42f8bb5c
Laz <lazgerasimov@gmail.com>
2022-02-08 13:01:41 +0000

    add

---
+++ 04-Functions/02. Exercise/02. Add and Subtract.js
@@ -0,0 +1,14 @@
+function addAndSubtract(num1, num2, num3) {
+
+    function add(firstNum, secondNum) {
+        return firstNum + secondNum;
+    }
+
+    let subtract = (addResult, thirdNumber) => addResult - thirdNumber;
+    let result = add(num1, num2);
+    let finalResult = subtract(result, num3);
+
+    console.log(finalResult)
+
+}
+addAndSubtract(23, 6, 10);
