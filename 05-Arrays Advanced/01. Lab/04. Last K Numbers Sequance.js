86771e9b7fc7c9ee09c277fd096a838e30a2f269
Laz <lazgerasimov@gmail.com>
2022-02-12 22:41:49 +0000

    add

---
+++ 05-Arrays Advanced/01. Lab/04. Last K Numbers Sequance.js
@@ -0,0 +1,17 @@
+function sequence(n, k) {
+
+    let result = [1];
+
+    for (let i = 0; i < n - 1; i++) {
+        let lastK = result.slice(-k);
+        let sum = 0;
+
+        for (let element of lastK) {     
+            sum += element;
+        }
+        result.push(sum);
+    }
+
+    console.log(result.join(" "));
+}
+sequence(6, 3)
