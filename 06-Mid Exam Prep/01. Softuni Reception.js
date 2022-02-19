147f6e67dfaaa0219152d1a4c45c5a0007c49757
Laz <lazgerasimov@gmail.com>
2022-02-19 22:55:55 +0000

    add

---
+++ 06-Mid Exam Prep/01. Softuni Reception.js
@@ -0,0 +1,22 @@
+function softUniReception (input) {
+
+    let e1 = Number(input[0]);
+    let e2 = Number(input[1]);
+    let e3 = Number(input[2]);
+    let students = Number(input[3]);
+    let totalStudentsServedPerHours = e1 + e2 + e3;
+    let time = 0; 
+
+    while (students > 0) {
+        time++; 
+        if (time % 4 == 0) {
+            students = students;
+        } else {
+            students -= totalStudentsServedPerHours;
+        }
+    }
+
+
+    console.log(`Time needed: ${time}h.`)
+}
+softUniReception(["5", "6", "4", "20"]);
