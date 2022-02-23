d1f214c011ee9ce34067b8142c9070d7cc4d3eaa
Laz <lazgerasimov@gmail.com>
2022-02-23 23:28:11 +0000

    add

---
+++ 07-Objects and Classes/01. Lab/1. Person Info.js
@@ -0,0 +1,11 @@
+function personInfo (firstName, lastName, age) {
+   
+    let result = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+     
+     return result;
+     
+ }
