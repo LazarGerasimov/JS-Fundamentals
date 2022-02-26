cdd8d8a901e841e5bad5c57242a9d45bfdddd08d
Laz <lazgerasimov@gmail.com>
2022-02-26 23:31:58 +0000

    add

---
+++ 07-Objects and Classes/02. Exercise/1. Employees.js
@@ -0,0 +1,24 @@
+function employees (input) {
+
+    let listOfEmployees = [];
+
+    class Employees {
+        constructor (name, length) {
+            this.name = name; 
+            this.length = length;
+        }
+    }
+    
+    for (let employee of input) {
+        let currentEmployee = new Employees(employee, employee.length);
+        listOfEmployees.push(currentEmployee);
+    }
+    
+    listOfEmployees.forEach((curEmployee) => 
+    console.log(`Name: ${curEmployee.name} -- Personal Number: ${curEmployee.length}`)
+    
+    )
+        
+    
+    
+}
