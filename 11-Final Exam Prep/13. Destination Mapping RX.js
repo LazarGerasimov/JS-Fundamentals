1ee3d4990f289e6b8c74a270f1af8ce544f9a109
Laz <lazgerasimov@gmail.com>
2022-03-21 08:28:06 +0000

    add

---
+++ 11-Final Exam Prep/13. Destination Mapping RX.js
@@ -0,0 +1,21 @@
+function destinationMapping(text) {
+
+    let result = [];
+    let points = 0;
+    let pattern = /(=|\/)([A-Z][A-Za-z]{2,})\1/g;
+    
+    let match = pattern.exec(text);
+
+   while (match != null) {
+       result.push(match[2]);
+        points += match[2].length;
+
+
+       match = pattern.exec(text)
+   }
+
+   console.log(`Destinations: ` + result.join(", "));
+   console.log(`Travel Points: ` + points);
+
+}
+destinationMapping("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=")
